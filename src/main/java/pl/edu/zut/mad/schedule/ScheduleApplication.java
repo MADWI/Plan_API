5ca848bc9efd2e8fbad3f7259a9112ad5220b269
Marek Macko <macko.marek93@gmail.com>
2017-09-09 16:27:51 +0200

    Initial commit

---
+++ src/main/java/pl/edu/zut/mad/schedule/ScheduleApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.zut.mad.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleApplication.class, args);
+	}
+}
