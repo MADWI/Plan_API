1cf5510d1db21f01b2c11df04117132fb2873c82
Łukasz Piskadło <lukasz.piskadlo@gmail.com>
2017-09-16 22:29:43 +0200

    Fix tests
    - Add database dialect for hibernate
    - Define environmental variables for tests

---
+++ src/test/java/pl/edu/zut/mad/schedule/ApiProperties.java
@@ -0,0 +1,9 @@
+package pl.edu.zut.mad.schedule;
+
+public final class ApiProperties {
+    public static final String DATABASE_USERNAME = "spring.datasource.username=test";
+    public static final String DATABASE_PASSWORD = "spring.datasource.password=test";
+
+    private ApiProperties() {
+    }
+}
--- src/test/java/pl/edu/zut/mad/schedule/ScheduleApplicationTests.java
+++ src/test/java/pl/edu/zut/mad/schedule/ScheduleApplicationTests.java
@@ -3,14 +3,19 @@ package pl.edu.zut.mad.schedule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(properties = {
+        ApiProperties.DATABASE_USERNAME,
+        ApiProperties.DATABASE_PASSWORD
+})
 public class ScheduleApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
