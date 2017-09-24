8d498b40c443f09742348d777a6f6deeef9c9d95
Łukasz Piskadło <lukasz.piskadlo@gmail.com>
2017-09-24 13:25:13 +0200

    Add production profile
    - use H2 database for development
    - use MySQL for production

--- src/test/java/pl/edu/zut/mad/schedule/ApiProperties.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.zut.mad.schedule;
-
-public final class ApiProperties {
-    public static final String DATABASE_USERNAME = "spring.datasource.username=test";
-    public static final String DATABASE_PASSWORD = "spring.datasource.password=test";
-
-    private ApiProperties() {
-    }
-}
