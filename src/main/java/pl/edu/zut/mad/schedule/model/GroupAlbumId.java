9120a8020fbe3690f87f720e4a7112f5037973e8
Łukasz Piskadło <lukasz.piskadlo@gmail.com>
2017-10-09 21:07:30 +0200

    Use lombok for equals and hashCode

--- src/main/java/pl/edu/zut/mad/schedule/model/GroupAlbumId.java
+++ src/main/java/pl/edu/zut/mad/schedule/model/GroupAlbumId.java
@@ -1,28 +1,13 @@
 package pl.edu.zut.mad.schedule.model;
 
+import lombok.EqualsAndHashCode;
+
 import java.io.Serializable;
 
 @SuppressWarnings("unused")
+@EqualsAndHashCode
 class GroupAlbumId implements Serializable {
 
     private Integer groupId;
     private String albumNumber;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        GroupAlbumId that = (GroupAlbumId) o;
-
-        if (groupId != null ? !groupId.equals(that.groupId) : that.groupId != null) return false;
-        return albumNumber != null ? albumNumber.equals(that.albumNumber) : that.albumNumber == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = groupId != null ? groupId.hashCode() : 0;
-        result = 31 * result + (albumNumber != null ? albumNumber.hashCode() : 0);
-        return result;
-    }
 }
