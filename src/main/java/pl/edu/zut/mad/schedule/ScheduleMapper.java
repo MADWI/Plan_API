d960aa44bb5bff6769a7dfbe98e21d1e7692b82a
Łukasz Piskadło <lukasz.piskadlo@gmail.com>
2017-10-26 21:10:36 +0200

    Change date and time format to epoch time

--- src/main/java/pl/edu/zut/mad/schedule/ScheduleMapper.java
+++ src/main/java/pl/edu/zut/mad/schedule/ScheduleMapper.java
@@ -9,6 +9,8 @@ import pl.edu.zut.mad.schedule.model.outer.TimeRange;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.List;
 import java.util.Map;
 
@@ -21,7 +23,7 @@ class ScheduleMapper {
 
     List<Day> daysFrom(final List<Schedule> schedule) {
         return schedule.stream()
-                .collect(groupingBy(s -> LocalDate.parse(s.getDate())))
+                .collect(groupingBy(this::epochDayFrom))
                 .entrySet()
                 .stream()
                 .map(this::dayFrom)
@@ -29,7 +31,13 @@ class ScheduleMapper {
                 .collect(toList());
     }
 
-    private Day dayFrom(Map.Entry<LocalDate, List<Schedule>> entry) {
+    private long epochDayFrom(final Schedule schedule) {
+        return LocalDate.parse(schedule.getDate())
+                .atStartOfDay()
+                .toEpochSecond(OffsetDateTime.now().getOffset());
+    }
+
+    private Day dayFrom(final Map.Entry<Long, List<Schedule>> entry) {
         return new Day(entry.getKey(), entry.getValue().stream()
                 .map(this::lessonFrom)
                 .sorted(comparing(lesson -> lesson.getTimeRange().getFrom()))
@@ -39,8 +47,16 @@ class ScheduleMapper {
     private Lesson lessonFrom(final Schedule schedule) {
         final Teacher teacher = new Teacher(schedule.getAcademicTitle(),
                 schedule.getName(), schedule.getSurname());
-        final TimeRange timeRange = new TimeRange(LocalTime.parse(schedule.getTimeFrom()),
-                LocalTime.parse(schedule.getTimeTo()));
+
+        final LocalDate date = LocalDate.parse(schedule.getDate());
+        final ZoneOffset zoneOffset = OffsetDateTime.now().getOffset();
+        final Long timeFrom = LocalTime.parse(schedule.getTimeFrom())
+                .atDate(date)
+                .toEpochSecond(zoneOffset);
+        final Long timeTo = LocalTime.parse(schedule.getTimeTo())
+                .atDate(date)
+                .toEpochSecond(zoneOffset);
+        final TimeRange timeRange = new TimeRange(timeFrom, timeTo);
 
         return new Lesson(schedule.getRoom(), schedule.getCourseType(), schedule.getSubject(),
                 schedule.getSemester(), schedule.getFaculty(), schedule.getFacultyAbbreviation(),
