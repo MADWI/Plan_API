514962afe67d71f64d0ffc25cb56fb37c11273d1
Łukasz Piskadło <lukasz.piskadlo@gmail.com>
2017-11-14 19:44:33 +0100

    Maybe fix exception misbehavior

--- src/main/java/pl/edu/zut/mad/schedule/ScheduleApplication.java
+++ src/main/java/pl/edu/zut/mad/schedule/ScheduleApplication.java
@@ -2,11 +2,15 @@ package pl.edu.zut.mad.schedule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
+@Configuration
 @SpringBootApplication
 public class ScheduleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScheduleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleApplication.class, args);
+    }
 }
