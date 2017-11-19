2a38da826fdb329d3dc8c82b78b618e9d092ffea
Łukasz Piskadło <lukasz.piskadlo@gmail.com>
2017-11-19 16:20:20 +0100

    Revert "Change date and time format to epoch time (#10)" (#11)
    This reverts commit 66ea975

--- src/main/java/pl/edu/zut/mad/schedule/model/outer/Day.java
+++ src/main/java/pl/edu/zut/mad/schedule/model/outer/Day.java
@@ -1,14 +1,17 @@
 package pl.edu.zut.mad.schedule.model.outer;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public class Day {
 
-    private final Long date;
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private final LocalDate date;
     private final List<Lesson> lessons;
 }
